adc0d477f05933ffdee9fc29c21914907d98084c
Abhijit <abhijits54@gmail.com>
2018-01-27 16:11:11 -0700

    Add files via upload

---
+++ reversearray.py
@@ -0,0 +1,11 @@
+def reverse_array(arr):
+	reverse_arr = [0]*len(arr)
+	j = (len(arr) - 1)
+	i = 0
+	while i < len(arr):
+		reverse_arr[i] = arr[j - i]
+		i = i + 1
+	return reverse_arr
+
+arr = [1,2,3,4]
+print reverse_array(arr)
